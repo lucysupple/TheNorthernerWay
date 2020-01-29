6a728c29627e36aa91a22ee8a49d12157703120c
Dominicode-s <60432849+Dominicode-s@users.noreply.github.com>
2020-01-29 13:30:15 +0000

    Scroll functionality added

--- js/JqueryScripts.js
+++ js/JqueryScripts.js
@@ -2,6 +2,16 @@ $(document).ready(function (){
 
     $(window).scroll(function (){
         console.log($(window).scrollTop())
+
+        if ($(window).scrollTop() > 280) {
+            $('#NavBar').addClass('navbar-fixed');
+          }
+
+        if ($(window).scrollTop() < 281) {
+            $('#NavBar').removeClass('navbar-fixed');
+          }
+
+
     });
 
 });
